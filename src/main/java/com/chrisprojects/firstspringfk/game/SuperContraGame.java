04fa73933f2d4c92899b4ab15cde8993bccc8bdb
Christian Mesina <chrmesina@icloud.com>
2023-05-05 12:06:13 -0700

    feat: loose coupling - interfaces

---
+++ src/main/java/com/chrisprojects/firstspringfk/game/SuperContraGame.java
@@ -0,0 +1,22 @@
+package com.chrisprojects.firstspringfk.game;
+
+public class SuperContraGame implements GamingConsole {
+
+    public void up() {
+        System.out.println("Jump");
+    }
+
+    public void down() {
+        System.out.println("Down");
+    }
+
+    public void left() {
+        System.out.println("Left");
+    }
+
+    public void right(){
+        System.out.println("right");
+    }
+
+
+}
---
+++ src/test/java/com/chrisprojects/firstspringfk/FirstSpringFkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chrisprojects.firstspringfk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSpringFkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
